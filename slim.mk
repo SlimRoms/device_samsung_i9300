d252e566df448b847bf425bd1a018ef74fbd7a50
Steven Holman <scuba156@gmail.com>
2013-11-08 18:34:30 +1100

    I9300: update dependencies and cleanup mk file
    Change-Id: I96252fbb1d24fa2253e50f66f164ecd4e45e4522

--- slim.mk
+++ slim.mk
@@ -1,21 +1,21 @@
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/slim/config/gsm.mk)
-
-# Release name
-PRODUCT_RELEASE_NAME := i9300
-
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common Slim stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/samsung/i9300/full_i9300.mk)
 
+# Inherit some common Slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/slim/config/gsm.mk)
+
 # Inherit torch settings
 $(call inherit-product, vendor/slim/config/common_ledflash.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := i9300
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i9300
