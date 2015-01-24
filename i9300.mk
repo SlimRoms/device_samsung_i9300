1d363178829d31e0c8446d4b8830c2cb503e1319
Christian Balster <christian.balster@gmail.com>
2015-01-24 16:07:39 +0100

    i9300: remove obsolete mobiledata.interfaces prop
    This property was used in GB to work around a hardcoded list of
    interfaces in MobileDataStateTracker and is no longer required.
    
     Author:    Christian Balster <christian.balster@gmail.com>
    Change-Id: Iaba910f52331079ab19ba05c2d2fc6586f895231

--- i9300.mk
+++ i9300.mk
@@ -88,7 +88,6 @@ $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.ril_class=SamsungExynos4RIL \
-    mobiledata.interfaces=pdp0,gprs,ppp0,rmnet0,rmnet1 \
     ro.telephony.call_ring.multiple=false \
     ro.telephony.call_ring.delay=3000
 
